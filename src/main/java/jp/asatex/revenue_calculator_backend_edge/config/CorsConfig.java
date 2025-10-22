5e96358c006ce5ddb56bcfc405dc8bef912a23af
Eric Niu <niuyuping@ydcrm.cn>
2025-10-22 21:47:26 +0800

    Fix bugs

--- src/main/java/jp/asatex/revenue_calculator_backend_edge/config/CorsConfig.java
+++ src/main/java/jp/asatex/revenue_calculator_backend_edge/config/CorsConfig.java
@@ -7,7 +7,6 @@ import org.springframework.web.reactive.config.EnableWebFlux;
 import org.springframework.web.reactive.config.WebFluxConfigurer;
 
 @Configuration
-@EnableWebFlux
 public class CorsConfig implements WebFluxConfigurer {
 
     @Override
